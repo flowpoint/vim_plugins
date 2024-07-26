85eaad44b5bf2c112c31faa70db7ffc5ec0bb5e4
flowpoint <flowpoint@protonmail.com>
2024-07-26 12:34:48 +0200

    cleanup for github

--- readme.md
+++ readme.md
@@ -1,1 +1,28 @@
+# nvim autocomplete plugin in <100 loc python
+
+only contains the boilerplate  so it llm inference can be easily added/customized
+the actual llm completions can be freely added as a simple python function
+
+it uses pynvim, neovims buildin menuone/popupmenu, completefunc, and preview window
+
+test it out locally:
+
+```
+python -m venv venv
+source venv/bin/activate
+pip install pynvim
+```
+
+start vim with the local plugin (avoids installing)
+
+```
 nvim -u ./vimrc2
+```
+
+the plugin itself sets the completefunc on vim start (bufenter)
+
+you might need to run:
+
+UpdateRemotePlugins to update the plugin
+
+tips: you can use :pc to close the preview window
--- rplugin/python3/pl.py
+++ rplugin/python3/pl.py
@@ -1,86 +1,65 @@
 import pynvim
 
-from inference_api.router import call_infer
+try:
+    # import you llm backends here
+    pass
+except ImportError:
+    pass
 
-#set omnifunc=Ofu1 ctrl-x ctrl-o
-# or
-#set completefunc=Ofu1 ctrl-x ctrl-u
 
-def inf(prompt):
-    max_length=200
-    system_prompt = 'only answer with succinct code. '
-    prompt += system_prompt
-    
-    model_name = 'mistralai/Mistral-7B-Instruct-v0.2'
-    res, control, debuginfo  = call_infer(prompt, max_length, model_name)
-    result = res.get(timeout=299)
-    return result['result']
+def suggest(prompt: str) -> list[str]:
+    ''' takes a prompt and suggests a list of completions '''
+    # call your llm backend here
+    max_length=300
+    system_prompt = 'Complete the code:'
+    prompt = f'{system_prompt} {prompt}'
+    completions = ['example_completion0', 'example_completion1', 
+                   'big completion'+'\nline'*20]
+    return completions
 
-def suggest(prompt):
-    if prompt.strip().endswith('hello'):
-        return ' world'
-    else:
-        return None
+
+def format_completion(prompt, completion, curpos):
+    lineno, colno = curpos
+    return {'word': prompt + ' ' + completion.split('\n')[0], 
+             'info': prompt + ' ' + completion,
+              'kind':'f',
+              'user_data': (curpos, completion),
+              }
 
 @pynvim.plugin
 class TestPlugin(object):
-
     def __init__(self, nvim):
         self.nvim = nvim
+        loop = nvim.loop
 
-    # then set omnifunc=Ofu1
-    @pynvim.function('Ofu1', sync=True)
-    def testfunction(self, args):
+    @pynvim.function('FlowComplete', sync=True)
+    def get_completion(self, args):
         findstart = args[0]
         base = args[1]
         win = self.nvim.current.window
-        buf = self.nvim.current.buffer
         curpos = win.cursor
-        l = self.nvim.current.line
 
+        # start completion at beginning of line (see neovim docs)
         if findstart == 1:
             return 0
         else:
-            #return list(base.split(' '))
-            res = inf(base)
-            lineno, colno = curpos
-            #buf[lineno+2] =  res
-            #return [base + ' ' + res, 'test']
-            compldict1 = {'word': base + ' ' + res.split('\n')[0], 
-                          'menu':"menutext", 
-                          'kind':'f',
-                          'user_data': (curpos, res),
-                          }
-            compldict2 = {'word': base}
-            return [compldict1, compldict2]
+            return [format_completion(base, x, curpos) for x in suggest(base)]
 
-    #nvim.subscribe
-
-    #@pynvim.command('TestCommand', nargs='*', range='')
-    @pynvim.function('TestFn', sync=True)
-    #def testcommand(self, args, range):
-    def testfunction2(self, args):
-        '''
-        self.nvim.current.line = ('Command with args: {}, range: {}'
-                                  .format(args, range))
-        '''
+    @pynvim.function('FlowCompleteInsert', sync=True)
+    def insert_completions(self, args):
         info = self.nvim.eval('complete_info()')
-        buf = self.nvim.current.buffer
+        completed_item = self.nvim.eval('v:completed_item')
 
+        # completion aborted
+        if completed_item == {}:
+            return 1
+
+        buf = self.nvim.current.buffer
         completions = info['items']
         selected = int(info['selected'])
-
         actual_completion = completions[selected]
-        da = actual_completion['user_data']
-        curpos, res = da
+        curpos, res = actual_completion['user_data']
         curline, curcol = curpos
-        '''
-        {'pum_visible': 1, 'mode': 'function', 'selected': 0, 
-        'items': [
-        {'word': "hello I'm an AI language model and don't have the ability to write or execute code directly. However,", 'menu': 'menutext', 'user_data': [[5, 0], "I'm an AI language model and don't have the ability to write or execute code directly. However,"], 'info': '', 'kind': 'f', 'abbr': ''}, 
-        {'word': 'hello', 'menu': '', 'user_data': '', 'info': '', 'kind': '', 'abbr': ''}]}
-        '''
-
         reslines = res.split('\n')[1:]
 
         for offset,resline in enumerate(reslines):
@@ -92,46 +71,12 @@ class TestPlugin(object):
                 oldrest = buf[nl:]
                 buf[nl] = str(resline)
                 buf[nl+1:] = oldrest
-        #buf[3] = str(args)
-        #print(args)
-        #buf[
         return 1
-
-    '''
-    @pynvim.command('TestComplete', pattern='*.py', eval='expand("<afile>")', sync=True)
-    def testcomplete(self, args, range):
-        #self.nvim.current.line = (.format(args, range))
-        pass
-    '''
-        
-    #@pynvim.autocmd('TestComplete2', pattern='CursorMovedI', eval='expand("<afile>")',sync=True)
-    #@pynvim.autocmd('CursorMovedI', pattern='*.py', eval='expand("<afile>")', sync=True)
-    #@pynvim.autocmd('CursorMovedI', pattern='*.py', eval='nvim_win', sync=True)
-    def testcomplete2(self, args):
-        #self.nvim.out_write('testcomplete is in')
-        #self.nvim.out_write(f'args: {args}')
-        print(args)
-        win = self.nvim.current.window
-        buf = self.nvim.current.buffer
-
-        # line, col
-        curpos = win.cursor
-        l = self.nvim.current.line
-        s = suggest(l)
-        #s = str(args)
-        src = self.nvim.new_highlight_source()
-        if s is not None:
-            #for li in range(
-            # group, line, colstart, colend
-            self.nvim.current.line = l + s
-            buf.add_highlight("String", curpos[0]-1, curpos[1], curpos[1] + len(s), src_id=src)
-        #self.nvim.out_write(f'args: {
 
     @pynvim.autocmd('BufEnter', pattern='*.py', eval='expand("<afile>")', sync=True)
     def on_bufenter(self, filename):
-        self.nvim.out_write('testplugin is in ' + filename + '\n')
-        self.nvim.command("set completefunc=Ofu1")
-        #self.nvim.command('autocmd! CompleteDonePre * TestCommand')
-        #self.nvim.command('autocmd! CompleteDonePre * TestCommand')
-        self.nvim.command('autocmd! CompleteDonePre * call TestFn()')
-        #self.nvim.command('autocmd! CompleteChanged * TestCommand')
+        self.nvim.command("set completefunc=FlowComplete")
+        self.nvim.command('autocmd! CompleteDonePre * call FlowCompleteInsert()')
+        # close the preview window after inserting
+        self.nvim.command('autocmd! CompleteDone * silent! pclose!')
+        #self.nvim.command('set completeopt="menuone,preview')
